cda91634525a679557da52c8abe946e109eae3b9
Eric Hankins <ehankins@rednovalabs.com>
2012-04-02 10:11:16 -0500

    Don't try to figure out the password, this is buggy and doesn't work. Just rely on it being sent in via the options if it's needed.

--- lib/capistrano/ssh.rb
+++ lib/capistrano/ssh.rb
@@ -80,7 +80,7 @@ module Capistrano
 
       begin
         connection_options = ssh_options.merge(
-          :password => password_value,
+          # :password => password_value,
           :auth_methods => ssh_options[:auth_methods] || methods.shift
         )
 
